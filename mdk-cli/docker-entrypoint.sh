4c53446ad16d2aff707453e68eb80cd05b354181
Martin Polehla <p0l0us@users.noreply.github.com>
2019-07-31 10:19:16 +0200

    Optimised Docker chain based on Alpine linux - Haxe + NodeJS - OpenJDK + Haxe + NodeJS - Ant + OpenJDK + Haxe + NodeJS - YHaxen + Ant + OpenJDK + Haxe + NodeJS - MDK-CLI

--- mdk-cli/docker-entrypoint.sh
+++ mdk-cli/docker-entrypoint.sh
@@ -13,4 +13,13 @@ echo "${SSH_PRIVATE_KEY_GZIP_BASE64}" | base64 -d | gunzip > /root/.ssh/id_rsa &
 
 cat ~/.ssh/known_hosts
 
+npm install -g git+ssh://git@stash.massiveinteractive.com:7999/mdk/mdk-cli-node
+
+# Expected params for mounting aws-iis
+# ${AWS_MOUNT_PATH} - samba share path
+# ${AWS_MOUNT_USERNAME} - samba share username
+# ${AWS_MOUNT_PASSWORD} - samba share password
+
+bash -c "mount -t cifs '${AWS_MOUNT_PATH}' /mnt/aws-iis -o username=${AWS_MOUNT_USERNAME},password=${AWS_MOUNT_PASSWORD},sec=ntlm"
+
 exec "$@"
