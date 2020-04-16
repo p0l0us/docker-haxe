074d8260ce6da186a017e295dcafa49c860134ef
Martin Polehla <p0l0us@users.noreply.github.com>
2020-04-16 14:43:17 +0200

    added safer aws mount condition to mdk-cli entrypoint

--- mdk-cli/bin/docker-entrypoint.sh
+++ mdk-cli/bin/docker-entrypoint.sh
@@ -10,15 +10,12 @@ then
 	echo "${SSH_KNOWN_HOSTS_GZIP_BASE64}" | base64 -d | gunzip > ~/.ssh/known_hosts
 fi
 
-
-
 # encode id_rsa by command:
 # cat ~/.ssh/id_rsa | gzip | base64 -w0
 if [ -n "${SSH_PRIVATE_KEY_GZIP_BASE64}" ]
 then
 	echo "${SSH_PRIVATE_KEY_GZIP_BASE64}" | base64 -d | gunzip > /root/.ssh/id_rsa && chmod 600 ~/.ssh/id_rsa
 fi
-
 
 if [ -n "${SSH_PRIVATE_KEY_GZIP_BASE64}" ] && [ -n "${SSH_KNOWN_HOSTS_GZIP_BASE64}" ]
 then
@@ -30,7 +27,7 @@ fi
 # ${AWS_MOUNT_USERNAME} - samba share username
 # ${AWS_MOUNT_PASSWORD} - samba share password
 
-if [ -z "${SSH_PRIVATE_KEY_GZIP_BASE64}" ]
+if [ -n "${AWS_MOUNT_PATH}" ] && [ -n "${AWS_MOUNT_USERNAME}" && [ -n "${AWS_MOUNT_PASSWORD}" ]
 then
 	echo "mount -t cifs '${AWS_MOUNT_PATH}' /mnt/aws-iis -o username=${AWS_MOUNT_USERNAME},password=${AWS_MOUNT_PASSWORD},sec=ntlm,rw,uid=0,gid=0,noperm"
 	bash -c "mount -t cifs '${AWS_MOUNT_PATH}' /mnt/aws-iis -o  username=${AWS_MOUNT_USERNAME},password=${AWS_MOUNT_PASSWORD},sec=ntlm,rw,uid=0,gid=0,noperm"
