d5c9322727fbd1391be9ce81bdeb6af24959f4e9
Martin Polehla <p0l0us@users.noreply.github.com>
2020-04-24 11:19:00 +0200

    typo fix

--- publish-all.sh
+++ publish-all.sh
@@ -11,5 +11,5 @@ docker push p0l0us/haxe:3.4-node-carbon-alpine
 docker push p0l0us/haxe:3.4-openjdk-12-node-carbon-alpine
 docker push p0l0us/haxe:3.4-ant-1.9.9-openjdk-12-node-carbon-alpine
 docker push p0l0us/haxe:3.4-yhaxen-0.0.50-ant-1.9.9-openjdk-12-node-carbon-alpine
-docker push p0l0us/haxe:3.4-mdk-cli-yhaxen-0.0.50-ant-1.9.9-openjdk-12-node-carbon-alpin
-docker push p0l0us/haxe:3.4-cordova-mdk-cli-yhaxen-0.0.50-ant-1.9.9-openjdk-12-node-carbon-alpinee
+docker push p0l0us/haxe:3.4-mdk-cli-yhaxen-0.0.50-ant-1.9.9-openjdk-12-node-carbon-alpine
+docker push p0l0us/haxe:3.4-cordova-mdk-cli-yhaxen-0.0.50-ant-1.9.9-openjdk-12-node-carbon-alpine
