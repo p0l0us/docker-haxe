ef6f34264f7eb2c5d0db0005cc8ed902a73255b4
Martin Polehla <p0l0us@users.noreply.github.com>
2020-04-24 12:00:46 +0200

    publish script reorganisation

--- publish-all.sh
+++ publish-all.sh
@@ -1,15 +1,25 @@
 #/bin/bash
 
 docker build node -t p0l0us/haxe:3.4-node-carbon-alpine
-docker build java-node -t p0l0us/haxe:3.4-openjdk-12-node-carbon-alpine
-docker build ant-java-node -t p0l0us/haxe:3.4-ant-1.9.9-openjdk-12-node-carbon-alpine
-docker build yhaxen-ant-java-node -t p0l0us/haxe:3.4-yhaxen-0.0.50-ant-1.9.9-openjdk-12-node-carbon-alpine
-docker build mdk-cli -t p0l0us/haxe:3.4-mdk-cli-yhaxen-0.0.50-ant-1.9.9-openjdk-12-node-carbon-alpine
-docker build cordova -t p0l0us/haxe:3.4-cordova-mdk-cli-yhaxen-0.0.50-ant-1.9.9-openjdk-12-node-carbon-alpine
-
 docker push p0l0us/haxe:3.4-node-carbon-alpine
+
+docker build java-node -t p0l0us/haxe:3.4-openjdk-12-node-carbon-alpine
 docker push p0l0us/haxe:3.4-openjdk-12-node-carbon-alpine
+
+docker build ant-java-node -t p0l0us/haxe:3.4-ant-1.9.9-openjdk-12-node-carbon-alpine
 docker push p0l0us/haxe:3.4-ant-1.9.9-openjdk-12-node-carbon-alpine
+
+docker build yhaxen-ant-java-node -t p0l0us/haxe:3.4-yhaxen-0.0.50-ant-1.9.9-openjdk-12-node-carbon-alpine
 docker push p0l0us/haxe:3.4-yhaxen-0.0.50-ant-1.9.9-openjdk-12-node-carbon-alpine
+
+docker build mdk-cli -t p0l0us/haxe:3.4-mdk-cli-yhaxen-0.0.50-ant-1.9.9-openjdk-12-node-carbon-alpine
 docker push p0l0us/haxe:3.4-mdk-cli-yhaxen-0.0.50-ant-1.9.9-openjdk-12-node-carbon-alpine
+
+docker build cordova -t p0l0us/haxe:3.4-cordova-mdk-cli-yhaxen-0.0.50-ant-1.9.9-openjdk-12-node-carbon-alpine
 docker push p0l0us/haxe:3.4-cordova-mdk-cli-yhaxen-0.0.50-ant-1.9.9-openjdk-12-node-carbon-alpine
+
+
+
+
+
+
