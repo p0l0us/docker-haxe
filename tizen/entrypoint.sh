0df4b26dd52668b59df1dc5d35b586b9a5450ca9
Jeroen Roumen <jeroen.roumen@deltatre.com>
2020-06-02 15:37:48 +0200

    if conditional for Tizen release plans

--- tizen/entrypoint.sh
+++ tizen/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -ex
 
-aws s3 cp s3://dazn-fe-build-data/tizen tizen --recursive
+if [$TIZEN_RELEASE -q true]
+then
+    aws s3 cp s3://dazn-fe-build-data/tizen tizen --recursive
+    tizen security-profiles add -n DAZN -a tizen/author.p12 -p ${SECURITY_PROFILES_PASSWD} -d tizen/distributor.p12 -dp ${SECURITY_PROFILES_PASSWD}
+fi    
 
-tizen security-profiles add -n DAZN -a tizen/author.p12 -p ${SECURITY_PROFILES_PASSWD} -d tizen/distributor.p12 -dp ${SECURITY_PROFILES_PASSWD}
+
