8de5e74a2bef697822e4a5a3a9962f022b472b4e
Jeroen Roumen <jeroen.roumen@deltatre.com>
2020-06-02 15:57:49 +0200

    fix if-statement

--- tizen/entrypoint.sh
+++ tizen/entrypoint.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 set -ex
 
-if [$TIZEN_RELEASE -q true]
-then
+if [ "$TIZEN_RELEASE" = true ] ; then
     aws s3 cp s3://dazn-fe-build-data/tizen tizen --recursive
     tizen security-profiles add -n DAZN -a tizen/author.p12 -p ${SECURITY_PROFILES_PASSWD} -d tizen/distributor.p12 -dp ${SECURITY_PROFILES_PASSWD}
-fi    
-
-
+fi
+  
