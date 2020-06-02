469df7971b95156e78e63bf643ad50832b4c553c
Jeroen Roumen <jeroen.roumen@deltatre.com>
2020-06-02 16:27:38 +0200

    remove space

--- tizen/entrypoint.sh
+++ tizen/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-if [ "$TIZEN_RELEASE" = true ] ; then
+if [ "$TIZEN_RELEASE" = true ]; then
     aws s3 cp s3://dazn-fe-build-data/tizen tizen --recursive
     tizen security-profiles add -n DAZN -a tizen/author.p12 -p ${SECURITY_PROFILES_PASSWD} -d tizen/distributor.p12 -dp ${SECURITY_PROFILES_PASSWD}
 fi
