83395d793f5c99a80fa23e4b92d11350f8727111
Jeroen Roumen <jeroen.roumen@deltatre.com>
2020-06-03 14:02:09 +0200

    download files into /tmp/tizen instead of /tizen

--- tizen/entrypoint.sh
+++ tizen/entrypoint.sh
@@ -2,6 +2,6 @@
 set -ex
 
 if [ "$TIZEN_RELEASE" = true ]; then
-    aws s3 cp $BUCKET_PATH tizen --recursive
-    tizen security-profiles add -n $PROFILE_NAME -a tizen/author.p12 -p ${SECURITY_PROFILES_PASSWD} -d tizen/distributor.p12 -dp ${SECURITY_PROFILES_PASSWD}
+    aws s3 cp $BUCKET_PATH /tmp/tizen --recursive
+    tizen security-profiles add -n $PROFILE_NAME -a /tmp/tizen/author.p12 -p ${SECURITY_PROFILES_PASSWD} -d /tmp/tizen/distributor.p12 -dp ${SECURITY_PROFILES_PASSWD}
 fi
