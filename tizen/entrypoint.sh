35f06a64cac9fe02fed4e78e328dd918b4376da7
Jeroen Roumen <jeroen.roumen@deltatre.com>
2020-06-03 10:10:42 +0200

    replace bucket path and profile name

--- tizen/entrypoint.sh
+++ tizen/entrypoint.sh
@@ -2,7 +2,6 @@
 set -ex
 
 if [ "$TIZEN_RELEASE" = true ]; then
-    aws s3 cp s3://dazn-fe-build-data/tizen tizen --recursive
-    tizen security-profiles add -n DAZN -a tizen/author.p12 -p ${SECURITY_PROFILES_PASSWD} -d tizen/distributor.p12 -dp ${SECURITY_PROFILES_PASSWD}
+    aws s3 cp $BUCKET_PATH tizen --recursive
+    tizen security-profiles add -n $PROFILE_NAME -a tizen/author.p12 -p ${SECURITY_PROFILES_PASSWD} -d tizen/distributor.p12 -dp ${SECURITY_PROFILES_PASSWD}
 fi
-  
