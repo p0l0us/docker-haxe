56364faa36bd92d3a55877df3d54dd209feba2c3
Jeroen Roumen <jeroen.roumen@deltatre.com>
2020-06-04 19:18:23 +0200

    fix env var expanding issue

--- tizen/entrypoint.sh
+++ tizen/entrypoint.sh
@@ -3,5 +3,5 @@ set -ex
 
 if [ "$TIZEN_RELEASE" = true ]; then
     aws s3 cp $BUCKET_PATH /tmp/tizen --recursive
-    tizen security-profiles add -n $PROFILE_NAME -a /tmp/tizen/author.p12 -p ${SECURITY_PROFILES_PASSWD} -d /tmp/tizen/distributor.p12 -dp ${SECURITY_PROFILES_PASSWD}
+    tizen security-profiles add -n $PROFILE_NAME -a /tmp/tizen/author.p12 -p $SECURITY_PROFILES_PASSWD -d /tmp/tizen/distributor.p12 -dp $SECURITY_PROFILES_PASSWD
 fi
