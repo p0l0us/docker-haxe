9d9321b70ec1c549a1602089c0cb93ab7d8882cd
Martin Polehla <p0l0us@users.noreply.github.com>
2020-06-08 17:47:46 +0200

    Update entrypoint.sh
    missing then fix

--- tizen/entrypoint.sh
+++ tizen/entrypoint.sh
@@ -2,6 +2,7 @@
 set -ex
 
 if [ -n "${TIZEN_AUTHOR_PASSWORD}" ] && [ -n "${TIZEN_DISTRIBUTOR_PASSWORD}" ]
+then
     aws s3 cp $BUCKET_PATH /tmp/tizen_profile --recursive
     tizen security-profiles add -n $PROFILE_NAME -a /tmp/tizen_profile/author.p12 -p "${TIZEN_AUTHOR_PASSWORD}" -d /tmp/tizen_profile/distributor.p12 -dp "${TIZEN_DISTRIBUTOR_PASSWORD}"
     rm -fr /tmp/tizen_profile
