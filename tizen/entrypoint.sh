6908b34dedc354710f91a5e744dd5f61019475a6
Martin Polehla <p0l0us@users.noreply.github.com>
2020-06-08 17:21:58 +0200

    Update entrypoint.sh
    Splitting tizen profiles passwords to author and distributor

--- tizen/entrypoint.sh
+++ tizen/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -ex
 
-if [ "$TIZEN_RELEASE" = true ]; then
-    aws s3 cp $BUCKET_PATH /tmp/tizen --recursive
-    tizen security-profiles add -n $PROFILE_NAME -a /tmp/tizen/author.p12 -p $SECURITY_PROFILES_PASSWD -d /tmp/tizen/distributor.p12 -dp $SECURITY_PROFILES_PASSWD
+if [ -n "${TIZEN_AUTHOR_PASSWORD}" ] && [ -n "${TIZEN_DISTRIBUTOR_PASSWORD}" ]
+    aws s3 cp $BUCKET_PATH /tmp/tizen_profile --recursive
+    tizen security-profiles add -n $PROFILE_NAME -a /tmp/tizen_profile/author.p12 -p "${TIZEN_AUTHOR_PASSWORD}" -d /tmp/tizen_profile/distributor.p12 -dp "${TIZEN_DISTRIBUTOR_PASSWORD}"
+    rm -fr /tmp/tizen_profile
 fi
