1cf3b58901cbbfbf6626311acce57cfdf41985f0
Martin Polehla <p0l0us@users.noreply.github.com>
2020-06-10 23:05:11 +0200

    Update entrypoint.sh

--- tizen/entrypoint.sh
+++ tizen/entrypoint.sh
@@ -8,8 +8,8 @@ if [ -n "${TIZEN_AUTHOR_PASSWORD}" ] && [ -n "${TIZEN_DISTRIBUTOR_PASSWORD}" ]
 then
     aws s3 cp $BUCKET_PATH ${CERT_PATH} --recursive
     tizen security-profiles add -n "${TIZEN_PROFILE_NAME}" -a "${CERT_PATH}/${TIZEN_AUTHOR_FILE}" -p "${TIZEN_AUTHOR_PASSWORD}" -d "${CERT_PATH}/${TIZEN_DISTRIBUTOR_FILE}" -dp "${TIZEN_DISTRIBUTOR_PASSWORD}"
-    sed -i -e '/author/ s/\(password="\).*pwd\(".*\)/\1${TIZEN_AUTHOR_PASSWORD}\2/g' ${TIZEN_PROFILE_FILE}
-    sed -i -e '/distributor/ s/\(password="\).*pwd\(".*\)/\1${TIZEN_DISTRIBUTOR_PASSWORD}\2/g' ${TIZEN_PROFILE_FILE}
+    sed -i -e "/author/ s/\(password=\"\).*pwd\(\".*\)/\1${TIZEN_AUTHOR_PASSWORD}\2/g" ${TIZEN_PROFILE_FILE}
+    sed -i -e "/distributor/ s/\(password=\"\).*pwd\(\".*\)/\1${TIZEN_DISTRIBUTOR_PASSWORD}\2/g" ${TIZEN_PROFILE_FILE}
 fi
 
 exec "$@"
