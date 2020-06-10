317e6f18cda5efe1f38e9b153314f6adc25efb36
Martin Polehla <p0l0us@users.noreply.github.com>
2020-06-10 12:07:17 +0200

    Update entrypoint.sh
    debug lines

--- tizen/entrypoint.sh
+++ tizen/entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -ex
 
+echo "AUTH: ${TIZEN_AUTHOR_PASSWORD}"
+echo "DIST: ${TIZEN_DISTRIBUTOR_PASSWORD}"
+
 if [ -n "${TIZEN_AUTHOR_PASSWORD}" ] && [ -n "${TIZEN_DISTRIBUTOR_PASSWORD}" ]
 then
     aws s3 cp $BUCKET_PATH /tmp/tizen_profile --recursive
-    tizen security-profiles add -n $PROFILE_NAME -a /tmp/tizen_profile/author.p12 -p "${TIZEN_AUTHOR_PASSWORD}" -d /tmp/tizen_profile/distributor.p12 -dp "${TIZEN_DISTRIBUTOR_PASSWORD}"
+    tizen security-profiles add -n ${TIZEN_PROFILE_NAME} -a /tmp/tizen_profile/author.p12 -p "${TIZEN_AUTHOR_PASSWORD}" -d /tmp/tizen_profile/distributor.p12 -dp "${TIZEN_DISTRIBUTOR_PASSWORD}"
     rm -fr /tmp/tizen_profile
 fi
