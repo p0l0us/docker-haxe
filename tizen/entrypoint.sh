7229efd6a663963f9b70928fa886de2fcacf54fe
Martin Polehla <p0l0us@users.noreply.github.com>
2020-06-10 22:27:48 +0200

    Update entrypoint.sh

--- tizen/entrypoint.sh
+++ tizen/entrypoint.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 set -e
 
+TIZEN_PROFILE_FILE="/home/massive/tizen-studio-data/profile/profiles.xml"
+
 if [ -n "${TIZEN_AUTHOR_PASSWORD}" ] && [ -n "${TIZEN_DISTRIBUTOR_PASSWORD}" ]
 then
     aws s3 cp $BUCKET_PATH /tmp/tizen_profile --recursive
-    tizen security-profiles add -n ${TIZEN_PROFILE_NAME} -a /tmp/tizen_profile/author.p12 -p "${TIZEN_AUTHOR_PASSWORD}" -d /tmp/tizen_profile/distributor.p12 -dp "${TIZEN_DISTRIBUTOR_PASSWORD}"
+    tizen security-profiles add -n "${TIZEN_PROFILE_NAME}" -a "${TIZEN_AUTHOR_FILE}" -p "${TIZEN_AUTHOR_PASSWORD}" -d "${TIZEN_DISTRIBUTOR_FILE}" -dp "${TIZEN_DISTRIBUTOR_PASSWORD}"
+    sed -i -e '/author/ s/\(password="\).*pwd\(".*\)/\1{TIZEN_AUTHOR_PASSWORD}\2/g' $TIZEN_PROFILE_FILE
+    sed -i -e '/distributor/ s/\(password="\).*pwd\(".*\)/\1${TIZEN_DISTRIBUTOR_PASSWORD}\2/g' $TIZEN_PROFILE_FILE
     rm -fr /tmp/tizen_profile
 fi
 
